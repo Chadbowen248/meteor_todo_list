fabd55c19946f880f23111dcb28365732e2b4dcc
Chad Bowen <Chadbowen248@gmail.com>
2016-06-27 12:41:31 -0500

    init commit- user can add items via mongo db. Edit function coming very soon, like in an hour

---
+++ imports/ui/body.js
@@ -0,0 +1,30 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import './body.html';
+
+
+
+Template.body.helpers( {
+  tasks() {
+    return Tasks.find({}, {sort: {createdAt: -1} });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // prevent default browser form submit
+    event.preventDefault();
+
+    // get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert tasks into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+    // clear form
+    target.text.value = '';
+  },
+});
